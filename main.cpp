b562b3244a993042135f7b541ca8cd23f521cb11
lorenzoluzzi <117952492+lorenzoluzzi@users.noreply.github.com>
2022-11-18 14:19:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,28 @@
 using namespace std;
 
 int main() {
+   
+  int opzione;
+   float lato_a,lato_b,result;
+   cin>>lato_a>>lato_b>>opzione;
+   
+   switch (opzione){
+      case 0:
+         result= (lato_a*lato_b)/2;
+         break;
+         case1:
+         result=lato_a*lato_a;
+         
+         break;
+      case 3:
+         result= lato_a*lato_b;
+         break;
+      default:
+         cout<< "opzione non valida";
+         break;
+        
+         
+         };
+   cout<< result;
    return 0;
 }
